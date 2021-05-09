fc0338815dfccdcdd1c9ec477ec50d5c79c2f3c9
ishaaan325jain <swati.912.jain@gmail.com>
2021-05-09 12:32:12 +0530

    array of birds

--- sketch.js
+++ sketch.js
@@ -7,6 +7,7 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
+var birds = [];
 
 var gameState = "onSling";
 var bg = "sprites/bg1.png";
@@ -14,6 +15,7 @@ var score = 0;
 
 function preload() {
     getBackgroundImg();
+    var  fly_sound = loadSound("sounds_rock_flying.mp3");
 }
 
 function setup(){
@@ -40,8 +42,15 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(200,50);
+    bird  = new Bird(200,50);
+    bird2 = new Bird(150,170);
+    bird3 = new Bird(100,170);
+    bird4 = new Bird(50,170);
 
+    birds.push(bird4);
+    birds.push(bird3);
+    birds.push(bird2);
+    birds.push(bird);
     //log6 = new Log(230,180,80, PI/2);
     slingshot = new SlingShot(bird.body,{x:200, y:50});
 }
@@ -75,6 +84,10 @@ function draw(){
     log5.display();
 
     bird.display();
+    bird2.display();
+    bird3.display();
+    bird4.display();
+
     platform.display();
     //log6.display();
     slingshot.display();    
@@ -82,19 +95,30 @@ function draw(){
 
 function mouseDragged(){
     if (gameState!=="launched"){
-        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+        c_bird = birds[birds.length-1];
+        Matter.Body.setPosition(c_bird.body, {x: mouseX , y: mouseY});
+        Matter.Body.applyForce(c_bird.body,c_bird.body.position,{x:5,y:-5});
+
     }
 }
 
 
 function mouseReleased(){
     slingshot.fly();
+    birds.pop();
     gameState = "launched";
+    fly_sound.play();
 }
 
 function keyPressed(){
-    if(keyCode === 32){
-       // slingshot.attach(bird.body);
+    if(keyCode === 32 && gameState === "launched"){
+        if(birds.length>=0){
+            c_bird = birds[birds.length-1];
+            Matter.Body.setPosition(c_bird.body,{x:200,y:50});
+            slingshot.attach(c_bird.body);
+            gameState = "onSling";
+        }
+        
     }
 }
 
